3f0ee41dbd477e66819e95d841fb8dd3561089c4
Berry Goudswaard <berry@xseeding.nl>
2015-05-12 18:08:58 +0200

    Update composer.json And make the tools compatible with PHP 5.3

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+$autoloadFiles = array(
+    __DIR__.'/../vendor/autoload.php',
+    __DIR__.'/../../../../vendor/autoload.php',
+);
+
+foreach ($autoloadFiles as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
